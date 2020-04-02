1ec8ea011cbb9f8fd714116b3a7e0118a0843af5
gongbin <214058830@qq.com>
2020-04-02 13:39:57 +0800

    2020-04-02

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# water_information_service-ui
-水利信息系统前端
+# service-ui
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Platform/Platform.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-view :username="username" :mail="mail" :flag="flag" :logo="logo" />
+</template>
+
+<script>
+export default {
+  props: {
+    username: String,
+    mail: String,
+    flag: String,
+    logo: String
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+  created() {}
+};
+</script>
+
+<style scoped>
+</style>
